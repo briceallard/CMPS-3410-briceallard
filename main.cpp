5e11b31b767555f2c3a3c75758a618c660677096
Shady Boukhary <sb199898.sb@gmail.com>
2019-01-02 20:26:44 +0200

    function test

--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+void printNumbers(int);
+
 int main() {
 	cout << "Hello World\n";
+	printNumbers(10);
 	return 0;
 
+}
+
+void printNumbers(int max) 
+{
+	for (int i = 0; i < max; i++)
+		cout << i << endl;
 }
