bb9cf9734ab6acdc409bf83a152b49178f6b73ac
Shady Boukhary <sb199898.sb@gmail.com>
2019-01-09 03:17:50 +0200

    testing for tables, precision, and strings

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ void printNumbers(int);
 int main() {
 	cout << "Hello World\n";
 	printNumbers(10);
+	cout << "-----------------------------\n20 23.22341\n";
+	cout << "------------------------\n";
+	cout << "The answer is: 9\n";
 	return 0;
 
 }
